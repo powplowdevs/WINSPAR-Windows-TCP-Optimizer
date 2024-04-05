b79a06a181318cccf518786420d0383ec3ece3c7
Ayoub Mohamed <kalidfawzia@gmail.com>
2024-04-04 22:53:09 -0400

    File object for storage analyzer

--- Src/Storage optimizer/main.cpp
+++ Src/Storage optimizer/main.cpp
@@ -1,19 +1,52 @@
 #include <windows.h>
 #include <iostream>
 #include <string>
+#include <locale>
+#include <codecvt>
 
 using namespace std;
 
-//Helper function to convert char array to wstring BROKEN!!!
-wstring MultiByteToWideCharString(const char* narrowStr) {
-    int size = MultiByteToWideChar(CP_ACP, 0, narrowStr, -1, nullptr, 0);
-    if (size == 0)
-        return L"";
-    wstring wideStr(size, 0);
-    MultiByteToWideChar(CP_ACP, 0, narrowStr, -1, &wideStr[0], size);
-    return wideStr;
-}
+//Helper function to convert char array to wstring   
+wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
 
+//Object for storage space ie a top level folder in a dir
+
+class Folder {
+    private:
+        std::string name;
+        std::string path;
+        int size; 
+    public:
+        Folder(const std::string& n, const std::string& p, int s) {
+            name = n;
+            path = p;
+            size = s;
+        }
+    
+        std::string getName(){
+            return name;
+        }
+        std::string getPath(){
+            return path;
+        }
+        int getSize() const{
+            return size;
+        }
+
+        void setName(const std::string& newName){
+            name = newName;
+        }
+        void setPath(const std::string& newPath){
+            path = newPath;
+        }
+        void setSize(int newSize){
+            size = newSize;
+        }
+};
+
+//**Find size of folder in bytes
+//**Pre: String folder path
+//**Post: ULONGLONG folder size in bytes
 ULONGLONG GetFolderSize(const wstring& folderPath) {
     WIN32_FIND_DATA findData;
     HANDLE hFind = FindFirstFileExW((folderPath + L"\\*").c_str(), FindExInfoStandard, &findData, FindExSearchLimitToDirectories, NULL, 0);
@@ -26,11 +59,10 @@ ULONGLONG GetFolderSize(const wstring& folderPath) {
                 fileSize.LowPart = findData.nFileSizeLow;
                 fileSize.HighPart = findData.nFileSizeHigh;
                 folderSize += fileSize.QuadPart;
-                cout << "hi" << endl;
             }
             else {
-                wstring wideString = MultiByteToWideCharString(findData.cFileName);
-                if (!wideString.empty()) {
+                wstring wideString = converter.from_bytes(findData.cFileName);
+                if (!wideString.empty() && wideString != L"." && wideString != L"..") {
                     wstring path = folderPath + L"\\" + wideString;
                     wcout << path << endl;
                     folderSize += GetFolderSize(path);
@@ -40,14 +72,19 @@ ULONGLONG GetFolderSize(const wstring& folderPath) {
         FindClose(hFind);
     }
     else {
-        cerr << "Error: " << GetLastError();
+        cerr << "Error: " << GetLastError() << endl;
     }
 
     return folderSize;
 }
 
+//**Scan folder and grab size of all parent dir and surface files
+//**Pre: String folder path
+//**Post: Dict with size of all parent dir or surface files
+
+
 int main() {
-    wstring folderPath = L"C:\\Users\\kalid.DESKTOP-TUS9USS\\Documents\\GitHub\\Capstone-optimizer";
+    wstring folderPath = L"C:\\$Recycle.Bin";
     ULONGLONG folderSize = GetFolderSize(folderPath);
     wcout << L"Folder size: " << folderSize / (1024 * 1024) << L" MB" << endl;
     return 0;
